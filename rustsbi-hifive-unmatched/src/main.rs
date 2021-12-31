af72241a9d1bad090ee42ed4a0c4bd233c143b5c
Nick Cao <nickcao@nichi.co>
2021-12-31 15:00:24 +0800

    insure ill is not delegated

--- rustsbi-hifive-unmatched/src/main.rs
+++ rustsbi-hifive-unmatched/src/main.rs
@@ -146,6 +146,7 @@ fn delegate_interrupt_exception() {
         medeleg::set_instruction_fault();
         medeleg::set_load_fault();
         medeleg::set_store_fault();
+        medeleg::clear_illegal_instruction();
         mie::set_mext();
         // 不打开mie::set_mtimer
         mie::set_msoft();
